2dfd17ed059b118f4c5c5163f59777958fa7b278
imkiran22 <kiran22292@gmail.com>
2020-05-10 19:01:51 +0530

    Random of 10000

--- packages/common/src/lib/Button/Button.tsx
+++ packages/common/src/lib/Button/Button.tsx
@@ -5,7 +5,7 @@ export interface Props {
   className: string;
 }
 export const Button: React.FC<Props> = ({ title, className}: Props) => {
-    const val = random(5000, 1).toString();
+    const val = random(10000, 1).toString();
     return (
       <button id={val} className={className}>{title}</button>
     )
