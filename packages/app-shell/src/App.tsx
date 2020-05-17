58169fac528e553b92b22077280cf23b8e7e385d
imkiran22 <kiran22292@gmail.com>
2020-05-17 20:42:26 +0530

    fsf

--- packages/app-shell/src/App.tsx
+++ packages/app-shell/src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-//import { Main, Button } from '@fedlight/common-components';
+import { Main, Button } from '@fedlight/common-components';
 import random from "lodash/random";
 
 function App() {
   const val = random(1);
-  import('@fedlight/common-components')
-  .then(module => console.log('MODULE LOADED', module))
-  .catch((e) => console.log('NOT ABLE TO LOAD FEDLIGHT COMMON COMPONENTS', e))
   return (
     <div className="App">
-      {/* <Main/>
-      <Button title="Kiran" className="Rock"/> */}
+      <Main/>
+      <Button title="Kiran" className="Rock"/>
     </div>
   );
 }
